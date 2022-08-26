39533269d680112ca9bdf298eff6c170d0a22a33
gitme8 <pavan.prime8@gmail.com>
2022-08-26 15:51:44 +0530

    JHi

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 resource "aws_instance" "demo-ins" {
   ami = "ami-0bb6af715826253bf"
